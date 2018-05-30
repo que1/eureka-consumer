a9fc91007221e0827f6c69dcc5cfaafb6ab2bd18
queyiwen <queyiwen@xunlei.com>
2018-05-30 19:41:58 +0800

    update

--- src/main/java/com/test/spring/cloud/eureka/consumer/api/ConsumerController.java
+++ src/main/java/com/test/spring/cloud/eureka/consumer/api/ConsumerController.java
@@ -3,10 +3,17 @@ package com.test.spring.cloud.eureka.consumer.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ProjectName: com.test.spring.cloud.eureka.consumer.api
@@ -31,14 +38,44 @@ public class ConsumerController {
 
     private static final Logger logger = LoggerFactory.getLogger(ConsumerController.class);
 
+    @LoadBalanced
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    @RequestMapping(value = "/call-hello", method = RequestMethod.GET)
     public String callSayHello() {
-        String result = this.restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
-        logger.info("/ribbon-consumer call say-hello: " + result);
-        return result;
+        ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("http://eureka-provider/hello", String.class);
+        String responstBody = responseEntity.getBody();
+        logger.info("/eureka-consumer, call eureka-provider api-hello: " + responstBody);
+        return responstBody;
+    }
+
+    @RequestMapping(value = "/call-textfilter-get", method = RequestMethod.GET)
+    public String callTextfilterGet() {
+        // 方法1：{1}&{2}, object...
+        // ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("http://eureka-provider/textfilter-get?param1={1}&param2={2}", String.class, "myParam1", "myParam2");
+        // 方法2：map
+        Map<String, String> requestMap = new HashMap<String, String>();
+        requestMap.put("param1", "myParam1");
+        requestMap.put("param2", "myParam2");
+        ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("http://eureka-provider/textfilter-get?param1={param1}&param2={param2}", String.class, requestMap);
+
+        String responstBody = responseEntity.getBody();
+        logger.info("/eureka-consumer, call eureka-provider api-textfilter-get: " + responstBody);
+        return responstBody;
+    }
+
+
+    @RequestMapping(value = "/call-textfilter-post", method = RequestMethod.GET)
+    public String callTextfilterPost() {
+        MultiValueMap<String, String> requestMap= new LinkedMultiValueMap<String, String>();
+        requestMap.add("param1", "myParam1");
+        requestMap.add("param2", "myParam2");
+        ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("http://eureka-provider/textfilter-post", requestMap, String.class);
+
+        String responstBody = responseEntity.getBody();
+        logger.info("/eureka-consumer, call eureka-provider api-textfilter-post: " + responstBody);
+        return responstBody;
     }
 
 }
